49860f6e3b3c100dd3809e3c4d40128d734a8578
andreadelsarto <delsadj@gmail.com>
2024-09-02 22:30:47 +0200

    info artist

--- lib/screens/song_list_screen.dart
+++ lib/screens/song_list_screen.dart
@@ -2,6 +2,8 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
+import 'package:path_provider/path_provider.dart';
+import 'image_service.dart';
 import 'player_screen.dart';
 import 'mini_player.dart';
 
@@ -24,17 +26,27 @@ class SongListScreen extends StatefulWidget {
 class _SongListScreenState extends State<SongListScreen> {
   final OnAudioQuery _audioQuery = OnAudioQuery();
   List<SongModel> _songs = [];
+  List<ArtistModel> _artists = [];
+  List<AlbumModel> _albums = [];
   bool _isLibraryView = true;
   bool _isArtistView = false;
   bool _isAlbumView = false;
+  Map<int, List<SongModel>> _albumSongs = {};
+  String? _selectedArtist;
+  Uint8List? _selectedArtistImage;
+  Uint8List? _selectedArtistBlurredImage; // Immagine sfocata dell'artista selezionato
+  List<int> _expandedAlbums = [];
+
 
   @override
   void initState() {
     super.initState();
     _fetchSongs();
+    _fetchArtists();
+    _fetchAlbums();
   }
 
-  void _fetchSongs() async {
+  Future<void> _fetchSongs() async {
     if (await _audioQuery.permissionsStatus() == false) {
       await _audioQuery.permissionsRequest();
     }
@@ -44,23 +56,45 @@ class _SongListScreenState extends State<SongListScreen> {
     });
   }
 
-  void _playAll() {
-    if (_songs.isNotEmpty) {
+  Future<void> _fetchArtists() async {
+    List<ArtistModel> artists = await _audioQuery.queryArtists();
+    setState(() {
+      _artists = artists;
+    });
+  }
+
+  Future<void> _fetchAlbums() async {
+    List<AlbumModel> albums = await _audioQuery.queryAlbums();
+    setState(() {
+      _albums = albums;
+    });
+  }
+
+  Future<void> _fetchSongsForAlbum(int albumId) async {
+    List<SongModel> songs = await _audioQuery.queryAudiosFrom(AudiosFromType.ALBUM_ID, albumId);
+    setState(() {
+      _albumSongs[albumId] = songs;
+    });
+  }
+
+  Future<void> _fetchArtistImage(String artistName, Color accentColor) async {
+    final images = await ImageService.fetchArtistImage(artistName, accentColor);
+    setState(() {
+      _selectedArtistImage = images['original'];
+      _selectedArtistBlurredImage = images['blurred'];
+    });
+  }
+
+  void _playAll(List<SongModel> songs) {
+    if (songs.isNotEmpty) {
       widget.audioPlayer.setAudioSource(
         ConcatenatingAudioSource(
-          children: _songs.map((song) {
+          children: songs.map((song) {
             return AudioSource.uri(Uri.parse(song.uri!));
           }).toList(),
         ),
       );
       widget.audioPlayer.play();
-    }
-  }
-
-  void _shuffleAll() {
-    if (_songs.isNotEmpty) {
-      widget.audioPlayer.setShuffleModeEnabled(true);
-      _playAll();
     }
   }
 
@@ -133,7 +167,7 @@ class _SongListScreenState extends State<SongListScreen> {
               ElevatedButton.icon(
                 icon: const Icon(Icons.play_arrow),
                 label: const Text('Play All'),
-                onPressed: _playAll,
+                onPressed: () => _playAll(_songs),
                 style: ElevatedButton.styleFrom(
                   foregroundColor: theme.colorScheme.onPrimary, backgroundColor: theme.colorScheme.primary,
                 ),
@@ -141,7 +175,10 @@ class _SongListScreenState extends State<SongListScreen> {
               ElevatedButton.icon(
                 icon: const Icon(Icons.shuffle),
                 label: const Text('Shuffle All'),
-                onPressed: _shuffleAll,
+                onPressed: () {
+                  widget.audioPlayer.setShuffleModeEnabled(true);
+                  _playAll(_songs);
+                },
                 style: ElevatedButton.styleFrom(
                   foregroundColor: theme.colorScheme.onPrimary, backgroundColor: theme.colorScheme.primary,
                 ),
@@ -188,12 +225,122 @@ class _SongListScreenState extends State<SongListScreen> {
   }
 
   Widget _buildArtistList() {
-    // Implement artist list view
-    return const Center(child: Text("Artist View"));
+    return ListView.builder(
+      itemCount: _artists.length,
+      itemBuilder: (context, index) {
+        final artist = _artists[index];
+        return ListTile(
+          title: Text(artist.artist),
+          subtitle: Text('${artist.numberOfAlbums} album(s), ${artist.numberOfTracks} track(s)'),
+          onTap: () async {
+            _selectedArtist = artist.artist;
+            await _fetchArtistImage(_selectedArtist!, Theme.of(context).colorScheme.primary); // Carica l'immagine sfocata
+            setState(() {
+              _isArtistView = false;
+              _isAlbumView = true; // Cambia vista per visualizzare gli album dell'artista selezionato
+            });
+          },
+        );
+      },
+    );
   }
 
   Widget _buildAlbumList() {
-    // Implement album list view
-    return const Center(child: Text("Album View"));
+    return Stack(
+      children: [
+        if (_selectedArtistBlurredImage != null) // Usa l'immagine sfocata come sfondo
+          Positioned.fill(
+            child: Image.memory(
+              _selectedArtistBlurredImage!,
+              fit: BoxFit.cover,
+              color: Colors.black.withOpacity(0.6),
+              colorBlendMode: BlendMode.darken,
+            ),
+          ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                _selectedArtist ?? '',
+                style: Theme.of(context).textTheme.headlineMedium?.copyWith(color: Colors.white),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _albums.length,
+                itemBuilder: (context, index) {
+                  final album = _albums[index];
+                  if (album.artist != _selectedArtist) return Container();
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ListTile(
+                        title: Text(album.album, style: TextStyle(color: Colors.white)),
+                        trailing: IconButton(
+                          icon: Icon(Icons.play_arrow, color: Colors.white),
+                          onPressed: () {
+                            _fetchSongsForAlbum(album.id).then((_) {
+                              _playAll(_albumSongs[album.id]!);
+                              // Aggiorna la vista del player con le canzoni dell'album
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => PlayerScreen(
+                                    audioFiles: _albumSongs[album.id]!,
+                                    initialIndex: 0,
+                                    audioPlayer: widget.audioPlayer,
+                                  ),
+                                ),
+                              );
+                            });
+                          },
+                        ),
+                        onTap: () {
+                          setState(() {
+                            if (_expandedAlbums.contains(album.id)) {
+                              _expandedAlbums.remove(album.id);
+                            } else {
+                              _expandedAlbums.add(album.id);
+                              if (!_albumSongs.containsKey(album.id)) {
+                                _fetchSongsForAlbum(album.id);
+                              }
+                            }
+                          });
+                        },
+                      ),
+                      if (_expandedAlbums.contains(album.id))
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16.0),
+                          child: Column(
+                            children: (_albumSongs[album.id] ?? []).map((song) {
+                              return ListTile(
+                                title: Text(song.title, style: TextStyle(color: Colors.white70)),
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => PlayerScreen(
+                                        audioFiles: _albumSongs[album.id]!,
+                                        initialIndex: _albumSongs[album.id]!.indexOf(song),
+                                        audioPlayer: widget.audioPlayer,
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
